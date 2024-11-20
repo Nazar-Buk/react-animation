e33a96e1e0098ba9169aef4cd70723749bfb4701
Nazar Buk <nazarbuk27@gmail.com>
2024-11-21 00:22:21 +0200

    Github Pages settings

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,27 @@ import Waves from "./pages/Waves";
 
 import "./styles/main.scss";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          element: <Home />,
+          index: true,
+        },
+        {
+          path: "/waves",
+          element: <Waves />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        element: <Home />,
-        index: true,
-      },
-      {
-        path: "/waves",
-        element: <Waves />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 function App() {
   return <RouterProvider router={router} fallbackElement={<>Loading...</>} />;
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/react-animation/", // YOUR REPO NAME HERE
+  //закоментуй base щоб дивитися проект локально
 });
